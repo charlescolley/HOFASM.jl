8468224f5ad40bdd776bbf487d3d372371848ef4
Charlie Colley <8076336+chuckcol@users.noreply.github.com>
2020-04-14 12:23:49 -0400

    fixed contraction tests, now implicit marginalization kron yields same results as new methods

--- src/contraction.jl
+++ src/contraction.jl
@@ -1,4 +1,13 @@
+"""
+"""
 
+
+
+"""----------------------------------------------------------------------------
+    Computes the contraction C(1,x) using the indicies of the tensor
+  non-zeros to compute the marginalization step and then the vector contraction
+  step after.
+----------------------------------------------------------------------------"""
 function implicit_kronecker_model_marginalization_contraction!(B_indices::Array{Array{Int,2},1},
                                                                B_vals::Array{Array{Float64,1},1},
                                                                H_indices::Array{Array{Int,2},1},
@@ -12,26 +21,15 @@ function implicit_kronecker_model_marginalization_contraction!(B_indices::Array{
        end
     end
 
-    #ei = Array{Int,1}(undef,6*size(Hn_indices,1)*length(Bn_vals))
-    #ej = Array{Int,1}(undef,6*size(Hn_indices,1)*length(Bn_vals))
-    #vals = Array{Float64,1}(undef,6*size(Hn_indices,1)*length(Bn_vals))
     for (Hn_indices,Bn_indices,Bn_vals) in zip(H_indices,B_indices,B_vals)
 
-        #println(Hn_indices)
-        #println(Bn_indices)
-        #println(Bn_vals)
         for i = 1:size(Hn_indices,1)
 
             for p in permutations([1,2,3])
                 _,i2,i3  = Hn_indices[i,p]
 
                 for j = 1:size(Bn_indices,1)
-                    #println(p)
                     _,j2,j3 = Bn_indices[j,p]
-                    #_,j2,j3 = Bn_indices[j,:]
-        #                j2 = Bn_indices[j,2]
-        #                j3 = Bn_indices[j,3]
-
                     y[(i2-1)*m + j2] +=  Bn_vals[j]*x[(i3-1)*m + j3]
 
                 end
@@ -40,7 +38,7 @@ function implicit_kronecker_model_marginalization_contraction!(B_indices::Array{
     end
 end
 
-
+#= TODO: verify this can be retired
 function implicit_kronecker_mode1_marginalization(Bn_indices::Array{Int,2},
     Bn_vals::Array{Float64,1},Hn_indices::Array{Int,2},n,m)
 
@@ -96,7 +94,8 @@ function implicit_kronecker_mode1_marginalization(Bn_indices::Array{Int,2},
     return sparse(ei,ej,vals,n*m,n*m)
     #return ei,ej,vals,n*m
 end
-
+=#
+#= TODO: verify this can be retired
 function sym_mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},m::Int)
     n,d = size(indices)
 
@@ -141,7 +140,7 @@ function sym_mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},
 
     return sparse(ei,ej,new_vals,m,m)
 end
-
+=#
 function mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},m::Int)
     n,d = size(indices)
 
@@ -163,6 +162,12 @@ function mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},m::I
 end
 
 
+"""----------------------------------------------------------------------------
+    Computes the contraction C(1,x) using list of pairs of the marginalized Hn
+  Bn pairs. Hn is typically quite sparse, so it's more efficient to figure out
+  which rows of the folded multiplicand should be multiplied to Bn, rather
+  than doing the explicit HnXBnᵀ operation.
+----------------------------------------------------------------------------"""
 function HOFASM_contraction!(tensor_pairs::Array{NTuple{2,SparseMatrixCSC{Float64,Int64}},1},
                              X_k::Array{Float64,2},X_k_1::Array{Float64,2}) #where {F <: AbstractFloat}
 
@@ -189,8 +194,17 @@ function HOFASM_contraction!(tensor_pairs::Array{NTuple{2,SparseMatrixCSC{Float6
 
 end
 
-function HOFASM_contraction!(tensor_pairs::Array{Tuple{Array{Int,1},Array{Int,1},SparseMatrixCSC{Float64,Int64}},1},
-                             X_k::Array{Float64,2},X_k_1::Array{Float64,2}) #where {F <: AbstractFloat}
+"""----------------------------------------------------------------------------
+    Computes the contraction C(1,x) using list of pairs of the marginalized Hn
+  Bn pairs. Hn is typically quite sparse, so it's more efficient to figure out
+  which rows of the folded multiplicand should be multiplied to Bn, rather
+  than doing the explicit HnXBnᵀ operation. The input here is a preprocessed
+  version of each of the Hn, so that the findnz operation isn't called
+  repeatedly.
+----------------------------------------------------------------------------"""
+function HOFASM_contraction!(tensor_pairs::Array{Tuple{Array{Int,1},Array{Int,1},Array{Float64,1},
+                                                       SparseMatrixCSC{Float64,Int64}},1},
+                             X_k::Array{Float64,2},X_k_1::Array{Float64,2})
 
     n,m = size(X_k_1)
     for i=1:n
@@ -199,12 +213,14 @@ function HOFASM_contraction!(tensor_pairs::Array{Tuple{Array{Int,1},Array{Int,1}
        end
     end
 
-    for (is,js,Bn) in tensor_pairs
+    for (is,js,vs,Bn) in tensor_pairs
         #is,js,_ = findnz(Hn)
-        for (i,j) in zip(is,js)
-            println(size(Bn))
-            println(size(X_k))
-            X_k_1[i,:]  .+= Bn*X_k[j,:]
+        for (i,j,v) in zip(is,js,vs)
+            if v > 1.0
+                X_k_1[i,:]  .+= v*Bn*X_k[j,:]
+            else
+                X_k_1[i,:]  .+= Bn*X_k[j,:]
+            end
         end
     end
 
@@ -213,7 +229,8 @@ end
 
 
 
-#TODO: need to verify output is correct
+#TODO: need to correct the output
+#TODO: check to see if this version is faster now that we don't explicitly compute HnX
 function SIMD_sparse_matrix_multiplication(Hn_matrices::Array{SparseMatrixCSC{Float64,Int64},1},
                                     Bn_matrices::Array{SparseMatrixCSC{Float64,Int64},1},
                                     X::Array{Float64,2},res_loc::Array{Float64,2})
@@ -273,3 +290,59 @@ function SIMD_sparse_matrix_multiplication(Hn_matrices::Array{SparseMatrixCSC{Fl
 
 
 end
+
+"""----------------------------------------------------------------------------
+    Computes the marginalization of a 3rd order tensor with respect to a
+  generalized transpose operation.
+----------------------------------------------------------------------------"""
+#allows the application of a permutation before contracting
+function perm_mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},
+                                    perm::Array{Int,1},m::Int)
+    n,d = size(indices)
+
+    ei = Array{Int,1}(undef, length(vals))
+    ej = Array{Int,1}(undef,length(vals))
+    new_vals = Array{Float64,1}(undef, length(vals))
+
+    index = 1
+    for i =1:n
+
+        ei[index] = indices[index,perm[2]]
+        ej[index] = indices[index,perm[3]]
+        new_vals[index] = vals[index]
+        index += 1
+
+    end
+
+    return sparse(ei,ej,new_vals,m,m)::SparseMatrixCSC{Float64,Int64}
+end
+
+function perm_marginalize(H_index,B_indices::Array{Int64,2},B_vals::Array{Float64,1},m::Int)
+
+    z = size(B_indices,1)
+    ei = zeros(Int,z*6)
+    ej = zeros(Int,z*6)
+    vals = zeros(Float64,z*6)
+    index = 1
+
+    for p in permutations([1,2,3])
+
+        i1,i2,i3 = H_index[p]
+
+        for (k,val) in zip(1:z,B_vals)
+
+            println(B_indices[k,:])
+            println("H_indices: $i1, $i2, $i3")
+
+            (_,j2,j3) = B_indices[k,p]
+            ei[index] = (i2-1)*m + j2
+            ej[index] = (i3-1)*m + j3
+            vals[index] = val
+            index += 1
+
+        end
+    end
+
+    return sparse(ei,ej,vals,m^2,m^2)
+end
+
--- test/test_code.jl
+++ test/test_code.jl
@@ -1,28 +1,38 @@
 
+using PyCall
+#special testing
+numpy = pyimport("numpy")
+function numpy_kron(A,B)
+    return numpy.kron(A,B)
+end
+
+ERROR_TOL = 1e-13
+
+
 #TODO: add in options for the tweaks made to make this testing possible
 function test_code()
-    n = 30
-    m = 30
+    n = 10
+    m = 10
     source = brute_force_triangles(rand(n,2))
     source[2] = (source[2][1],source[1][2]) #make a duplicate angle
     target = brute_force_triangles(rand(m,2))
 
-    # one triangle for now
-    H_indices, H_vals,B_indices,B_vals = build_index_and_bases_tensors(source, target, 5.0)
-    #return H_indices, H_vals,B_indices,B_vals
-    H_marg_tens = [sym_mode1_marginalization(H_ind,H_vals,n) for (H_ind,H_vals) in zip(H_indices, H_vals)]
-    B_marg_tens = [mode1_marginalization(B_ind,B_vals,m) for (B_ind,B_vals) in zip(B_indices, B_vals)]
-    HOFASM_marg_ten = numpy_kron(H_marg_tens[1],B_marg_tens[1])
+    H_indices, H_vals,B_indices,B_vals =
+        build_index_and_bases_tensors(source, target, 5.0,test_mode=true)
+
+    #H_marg_tens = [sym_mode1_marginalization(H_ind,H_vals,n) for (H_ind,H_vals) in zip(H_indices, H_vals)]
+    #B_marg_tens = [mode1_marginalization(B_ind,B_vals,m) for (B_ind,B_vals) in zip(B_indices, B_vals)]
+    #HOFASM_marg_ten = numpy_kron(H_marg_tens[1],B_marg_tens[1])
 
     #HOFASM_perm_marg_ten = Matrix(perm_marginalize2(H_indices[1],B_indices[1],B_vals[1],m))
     HOFASM_perm_marg_ten = HOFASM_mode1_marg(H_indices,B_indices,B_vals,m,n)
 
-    pairs = Make_HOFASM_tensor_pairs(H_indices,B_indices,B_vals,m,n)
+    #pairs = Make_HOFASM_tensor_pairs(H_indices,B_indices,B_vals,m,n)
 
-    HOM_indices, HOM_vals = produce_HOM_tensor(source,target,m,tol=1e-16)
+    HOM_indices, HOM_vals = produce_HOM_tensor(source,target,m,tol=1e-16,test_mode=true)
     HOM_marg_ten = Matrix(sym_mode1_marginalization(HOM_indices,HOM_vals,n*m))
 
-   #return HOFASM_perm_marg_ten, HOM_marg_ten
+    #return HOFASM_perm_marg_ten, HOM_marg_ten
     #return HOM_marg_ten, H_indices, H_vals,B_indices,B_vals
     #check for the same values
 
@@ -30,13 +40,13 @@ function test_code()
 
     #symmetry check
 
-    @assert norm(HOM_marg_ten - HOM_marg_ten')/norm(HOM_marg_ten) < 1e-14
-    println(norm(HOFASM_marg_ten - HOFASM_marg_ten')/norm(HOFASM_marg_ten))
-    @assert norm(HOFASM_perm_marg_ten - HOFASM_perm_marg_ten')/norm(HOFASM_perm_marg_ten) < 1e-14
+    @assert norm(HOM_marg_ten - HOM_marg_ten')/norm(HOM_marg_ten) < ERROR_TOL
+    #println(norm(HOFASM_marg_ten - HOFASM_marg_ten')/norm(HOFASM_marg_ten))
+    @assert norm(HOFASM_perm_marg_ten - HOFASM_perm_marg_ten')/norm(HOFASM_perm_marg_ten) < ERROR_TOL
 
     #equality check
     println(norm(HOM_marg_ten - HOFASM_perm_marg_ten) / norm(HOFASM_perm_marg_ten))
-    @assert norm(HOM_marg_ten - HOFASM_perm_marg_ten)/norm(HOFASM_perm_marg_ten) < 1e-14
+    @assert norm(HOM_marg_ten - HOFASM_perm_marg_ten)/norm(HOFASM_perm_marg_ten) < ERROR_TOL
 
 end
 
@@ -49,22 +59,30 @@ function test_contraction(n::Int=30,m::Int=30,bin_size::Float64=5.0)
 
     # one triangle for now
     H_indices, H_vals,B_indices,B_vals = build_index_and_bases_tensors(source, target, bin_size)
+   # return H_indices, H_vals,B_indices,B_vals, source, target
     pairs = Make_HOFASM_tensor_pairs(H_indices,B_indices,B_vals,m,n)
-    println("built pairs")
 
-    x = rand(n*m)
-    X = Matrix(reshape(x,n,m)')
-    Y = rand(n,m)
-    preprocessed_pairs = [(findnz(H)[1],findnz(H)[2],B) for (H,B) in pairs]
+    println("built $(length(pairs)) pairs")
+
+    x = ones(n*m)
+    X = Matrix(reshape(x,m,n)') #julia defaults to col major formatting
+    Y1 = rand(n,m)
+
+    t1 = @timed HOFASM_contraction!(pairs,X,Y1)
+    println("HOFASM_contraction ran in $(t1[2])")
+
+
+    Y2 = rand(n,m)
+    preprocessed_pairs = [(findnz(H)...,B) for (H,B) in pairs]
+    #preprocessed_pairs = [(findnz(H)...,B) for (H,B) in pairs]
     #return preprocessed_pairs,X,Y
-    t1 = @timed HOFASM_contraction!(preprocessed_pairs,X,Y)
-    t1 = t1[2]
-    println("HOFASM_contraction ran in $t1")
+    t2 = @timed HOFASM_contraction!(preprocessed_pairs,X,Y2)
+    println("preprocessed HOFASM_contraction ran in $(t2[2])")
+
 
     implit_marg_kron_y = rand(n*m)
-    t2 = @timed implicit_kronecker_model_marginalization_contraction!(B_indices,B_vals,H_indices,x,implit_marg_kron_y,n,m)
-    t2 = t2[2]
-    println("HOFASM_implicit_marg_kron_contraction ran in $t2")
+    t3 = @timed implicit_kronecker_model_marginalization_contraction!(B_indices,B_vals,H_indices,x,implit_marg_kron_y,n,m)
+    println("HOFASM_implicit_marg_kron_contraction ran in $(t3[2])")
 
     #HOM_indices, HOM_vals = produce_HOM_tensor(source,target,m,tol=1e-5)
     #HOM_marg_ten = Matrix(sym_mode1_marginalization(HOM_indices,HOM_vals,n*m))
@@ -72,20 +90,21 @@ function test_contraction(n::Int=30,m::Int=30,bin_size::Float64=5.0)
     #t3 = t3[2]
     #println("HOM ran in time $t3")
 
-    return implit_marg_kron_y , Y
+  #  return Matrix(reshape(implit_marg_kron_y,m,n)'), Y
+    diff1 = norm(Y1 - Y2)/norm(Y2)
+    println(diff1)
+    @assert diff1 < ERROR_TOL
 
-    diff = norm(reshape(implit_marg_kron_y,n,m) - Y)/norm(Y)
-    println(diff)
-    @assert diff < 1e-14
+    diff2 = norm(Matrix(reshape(implit_marg_kron_y,m,n)') - Y2)/norm(Y2)
+    println(diff2)
+    @assert diff2 < ERROR_TOL
+
+    println("performance ratio impli:new = $(t2[2]/t1[2])")
+
 end
 
 
 
-#special testing
-#@pycall numpy
-#function numpy_kron(A,B)
-#    return numpy.kron(A,B)
-#end
 
 
 function brute_force_tensor(H_indices,B_indices,B_vals,n,m)
@@ -110,55 +129,14 @@ function brute_force_tensor(H_indices,B_indices,B_vals,n,m)
     return final_indices, final_vals
 end
 
-function perm_marginalize(H_index,B_indices::Array{Int64,2},B_vals::Array{Float64,1},m::Int)
 
-    z = size(B_indices,1)
-    ei = zeros(Int,z*6)
-    ej = zeros(Int,z*6)
-    vals = zeros(Float64,z*6)
-    index = 1
+function HOFASM_mode1_marg(H_indices::Array{Array{Int64,2},1},B_indices::Array{Array{Int64,2},1},
+                            B_vals::Array{Array{Float64,1},1},m::Int,n::Int)
 
-    for p in permutations([1,2,3])
-
-        i1,i2,i3 = H_index[p]
-
-        for (k,val) in zip(1:z,B_vals)
-
-            println(B_indices[k,:])
-            println("H_indices: $i1, $i2, $i3")
-
-            (_,j2,j3) = B_indices[k,p]
-            ei[index] = (i2-1)*m + j2
-            ej[index] = (i3-1)*m + j3
-            vals[index] = val
-            index += 1
-
-        end
-    end
-
-    return sparse(ei,ej,vals,m^2,m^2)
+        return sum([sum(
+                [numpy_kron(perm_mode1_marginalization(H_idx,ones(size(H_idx,1)),p,n),
+                            perm_mode1_marginalization(B_idx,v,p,m)
+                            )
+                            for p in permutations((1,2,3))
+                ]) for (H_idx,B_idx,v) in zip(H_indices,B_indices,B_vals)])
 end
-
-
-#allows the application of a permutation before contracting
-function perm_mode1_marginalization(indices::Array{Int,2},vals::Array{Float64,1},
-                                    perm::Array{Int,1},m::Int)
-    n,d = size(indices)
-
-    ei = Array{Int,1}(undef, length(vals))
-    ej = Array{Int,1}(undef,length(vals))
-    new_vals = Array{Float64,1}(undef, length(vals))
-
-    index = 1
-    for i =1:n
-
-        ei[index] = indices[index,perm[2]]
-        ej[index] = indices[index,perm[3]]
-        new_vals[index] = vals[index]
-        index += 1
-
-    end
-
-    return sparse(ei,ej,new_vals,m,m)::SparseMatrixCSC{Float64,Int64}
-end
-
