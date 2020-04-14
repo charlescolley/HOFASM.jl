998288f5dc5fa552128f8cdd21063db28c03ef44
Charlie Colley <8076336+chuckcol@users.noreply.github.com>
2020-04-14 12:26:20 -0400

    added in testing mode to HOM code, testing mode added to HOFASM in previous commit (clean up later)

--- src/HOM.jl
+++ src/HOM.jl
@@ -66,7 +66,7 @@ end
 -------------------------------------------------------------------------"""
 function produce_HOM_tensor(image1_triangles::Array{Tuple{Tuple{Int,Int,Int},Tuple{F,F,F}},1},
                             image2_triangles::Array{Tuple{Tuple{Int,Int,Int},Tuple{F,F,F}},1},
-                            m::Int; tol::Float64=1e-5) where {F <: AbstractFloat}
+                            m::Int; tol::Float64=1e-5,test_mode=false) where {F <: AbstractFloat}
     max_tris  = size(image1_triangles,1)*size(image2_triangles,1)*6
 
     HOM_indices = Array{Int64,2}(undef,max_tris,3)
@@ -77,7 +77,7 @@ function produce_HOM_tensor(image1_triangles::Array{Tuple{Tuple{Int,Int,Int},Tup
         for (indices,img2_angles) in image2_triangles
             for ((i2,j2,k2),angle_perm) in zip(permutations(indices),permutations(img2_angles))
 
-                val = HOM_angle_diff(img1_angles,tuple(angle_perm...),1.0)
+                val = HOM_angle_diff(img1_angles,tuple(angle_perm...),1.0,test_mode=test_mode)
                 if val > tol
                     HOM_indices[hyperedge_index,1] = (i1 -1)*m + i2
                     HOM_indices[hyperedge_index,2] = (j1 -1)*m + j2
@@ -94,10 +94,13 @@ function produce_HOM_tensor(image1_triangles::Array{Tuple{Tuple{Int,Int,Int},Tup
 
 end
 
-function HOM_angle_diff(angles1::Tuple{F,F,F}, angles2::Tuple{F,F,F}, epsilon::F) where {F <: AbstractFloat}
+function HOM_angle_diff(angles1::Tuple{F,F,F}, angles2::Tuple{F,F,F}, epsilon::F;test_mode=false) where {F <: AbstractFloat}
     a1,a2,a3 = angles1
     ap1,ap2,ap3 = angles2
-    return ((a1 - ap1)^2+(a2 - ap2)^2+(a3 - ap3)^2) / epsilon^2
-    #return exp(-((a1 - ap1)^2+(a2 - ap2)^2+(a3 - ap3)^2) / epsilon^2)
+    if test_mode
+        return ((a1 - ap1)^2+(a2 - ap2)^2+(a3 - ap3)^2) / epsilon^2
+    else
+        return exp(-((a1 - ap1)^2+(a2 - ap2)^2+(a3 - ap3)^2) / epsilon^2)
+    end
 end
 
